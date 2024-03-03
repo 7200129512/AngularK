fc48c412cf21fcb911b9dcb39e8c5151e9dd4e6f
7200129512 <35841959+7200129512@users.noreply.github.com>
2024-03-03 22:05:21 +0530

    Angular project added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'Home',component:HomeComponent},
+    {path:'login',component:LoginComponent},
+    {path:'register',component:RegisterComponent},
+    {path:'about',component:AboutComponent},
+    {path:'contact',component:ContactComponent},
+];
